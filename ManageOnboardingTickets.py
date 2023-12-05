e078fa947690422b218fb1434324100373a5e89d
AnthonyMontano <151967473+AnthonyMontano@users.noreply.github.com>
2023-12-04 22:04:17 -0600

    Update ManageOnboardingTickets.py
    added the main idiom to ensure the Token Manager code doesn't when imported

--- ManageOnboardingTickets.py
+++ ManageOnboardingTickets.py
@@ -1,145 +1,151 @@
-from TokenManager import TokenManager
 import requests
-import re
+from dotenv import load_dotenv
 import json
-from bs4 import BeautifulSoup
 
-"""The Purpose of this class it to access Onboarding ticket data and store it in a understandable way for comprehension and future scripting """
-"""Need to implement a method or class that makes changes to the tickets such as changing the status to in progress"""
-class ManageOnboardingTickets:
+if __name__ == '__main__':
+    print('This class is not meant to be ran directly')
+else:
 
-    def __init__(self):
-        self.url = "https://desk.zoho.com/api/v1/tickets"
-        self.headers = {"orgId":"EnterOrgID"}
-        self.params = {'departmentIds': 'EnterDepartmentId', 'status':'enterticketstatushere'}
-        self.ticketdata = ""
-        self.ticketIds = []
-        self.jsoncontent = ""
-        self.jsonstring = ""
-        self.content = ""
-    
+    with open('OnboardingConfigFileTest.json' ,'r') as file:
+        config_data = json.load(file)
 
-    def getopentickets(self):
-        try:
-            with open('Enter Filename here', 'r') as file:
-                access_token_str = file.read()
-            self.headers['Authorization'] = f"Zoho-oauthtoken {access_token_str}"
-            response = requests.get(url = self.url, headers = self.headers, params = self.params)
-            status_code = response.status_code
-            match status_code:
-                case 200:
-                    print('Status Code: Ok... Grabbing Ticket Numbers and IDs....')
-                    jsondata = response.json()
-                    for ticket in jsondata.get('data',[]):
-                        ticket_id = ticket.get('id')
-                        ticket__number = ticket.get('ticketNumber')
-                        self.ticketIds.append(ticket_id)
-                        self.ticketdata += f"Ticket Number: {ticket__number}\nTicket ID: {ticket_id}\n\n"
-                case 201:
-                    print('Status Code: Created')
-                case 204:
-                    print('Status Code: No content')
-                case 400:
-                    print('Status Code: Bad request')
-                case 401:
-                    print('Status Code: Unauthorized... Grabbing a new token... ')
-                    instance_a = TokenManager()
-                    instance_a.call_new_access_token()
-                    print("Renewed token, Now grabbing Ticket numbers and IDs....")
-                    self.getopentickets()
-                case 403:
-                    print('Status Code: Forbidden (Unauthorised access)')
-                case 404:
-                    print('Status Code: URL not found')
-                case 405:
-                    print('Status Code: Method not allowed (Method called is not supported for the API invoked)')
-                case 413:
-                    print('Status Code: Payload Too Large')
-                case 415:
-                    print('Status Code: Unsupported Media Type')
-                case 422:
-                    print('Status Code: Unprocessable Entity')
-                case 429:
-                    print('Status Code: Too Many Requests')
-                case 500:
-                    print('Status Code: Internal error')
-        except requests.exceptions.ConnectionError:
-            print("The internet is not accessible at the moment please try again later.")
+    read_access_token_url = config_data['read_access_token_url']
+    read_access_token_params = config_data['read_access_token_params']
+    update_access_token_url = config_data['update_access_token_url']
+    update_access_token_params = config_data['update_access_token_params']
 
+    class TokenManagerTest:
 
-    def GetOnboardticketContent(self):
-        startofcontenturl = "https://desk.zoho.com/api/v1/tickets/"
-        endofcontenturl = "/latestThread"
-        with open('Enter File Here', 'r') as file:
-                access_token_str = file.read()
-        #
-        # access_token_str = open('Access_Token_Text', 'r').read()
-        self.headers['Authorization'] = f"Zoho-oauthtoken {access_token_str}"
-        for ticketid in self.ticketIds:
-            contenturl = f"{startofcontenturl}{ticketid}{endofcontenturl}"
-            response = requests.get(contenturl, headers=self.headers)
-            content_status_code = response.status_code
-            match content_status_code:
-                case 200:
-                    print('Status Code: Ok... Grabbing Ticket Content....')
-                    self.jsoncontent = json.loads(response.text)
-                    ticket.PrepData()
-                case 201:
-                    print('Status Code: Created')
-                case 204:
-                    print('Status Code: No content')
-                case 400:
-                    print('Status Code: Bad request')
-                case 401:
-                    print('Status Code: Unauthorized... Grabbing a new token...')
-                    instance_d = TokenManager()
-                    instance_d.call_new_access_token()
-                    ticket.GetOnboardticketContent()
-                case 403:
-                    print('Status Code: Forbidden (Unauthorised access)')
-                case 404:
-                    print('Status Code: URL not found')
-                case 405:
-                    print('Status Code: Method not allowed (Method called is not supported for the API invoked)')
-                case 413:
-                    print('Status Code: Payload Too Large')
-                case 415:
-                    print('Status Code: Unsupported Media Type')
-                case 422:
-                    print('Status Code: Unprocessable Entity')
-                case 429:
-                    print('Status Code: Too Many Requests')
-                case 500:
-                    print('Status Code: Internal error')
-
-
-    def PrepData(self):
-        """Need to go back and really learn REGEX as this is a pivotal part to the process. This part took the longest to research for me"""
-        a = self.jsoncontent['content']
-        text=json.dumps(a, sort_keys=True, indent=4)
-        soup = BeautifulSoup(text, 'html.parser')
-        text_content = re.sub(r'<.*?>', '', soup.get_text(separator='\n'))       
-        contact_info_pattern = re.compile(r'Contact Info:(.*?)IT Equipment:', re.DOTALL)
-        supervisor_info_pattern = re.compile(r'Supervisor:(.*?)Location Info:', re.DOTALL)
-        location_info_pattern = re.compile(r'Location Info:(.*?)Recorded Date:', re.DOTALL)
-        recorded_date_pattern = re.compile(r'Recorded Date:(.*?)Notes:', re.DOTALL)
-        contact_info_match = re.search(contact_info_pattern, text_content)
-        supervisor_info_match = re.search(supervisor_info_pattern, text_content)
-        location_info_match = re.search(location_info_pattern, text_content)
-        recorded_date_match = re.search(recorded_date_pattern, text_content)    
-        str = (f"{contact_info_match.group(1).strip()}\n{supervisor_info_match.group(1).strip()}\n{location_info_match.group(1).strip()}\nEntry Date:{recorded_date_match.group(1).strip()}")
-        lines = str.split('\n')
-        data_dict = {}
-        for line in lines:
-            parts = line.split(':')
-            key = parts[0].strip()
-            value = parts[1].strip() if len(parts) > 1 else '' 
-            data_dict[key] = value
-        with open("Enter File Here", 'a') as f:  
-            for key, value in data_dict.items():  
-                f.write('%s:%s\n' % (key, value))
-            f.write('\n')
+        def __init__(self, scope):
+            """Assigning the variables for my API calls"""
+            self.read_access_token_url = read_access_token_url
+            self.read_access_token_params = read_access_token_params
+            self.update_access_token_url = update_access_token_url
+            self.update_access_token_params = update_access_token_params
+            self.scope = scope
+            
         
-ticket = ManageOnboardingTickets()
-ticket.getopentickets()
-ticket.GetOnboardticketContent()
+        def call_new_access_token(self):
+            """Call to autostore token if OK is received"""
+            """Need to implement switch statement to go through the codes"""
+            match self.scope:
+                case {'scope': 'Desk.tickets.READ'}:
+                    self.read_access_token_params.update(self.scope)
+                    response = requests.post(self.read_access_token_url, self.read_access_token_params)
+                    statuscode = response.status_code
+                    jsondata = response.json()
+                    match statuscode:
+                        case 200:
+                            
+                            print('Status Code: OK')
+                            access_token_read = jsondata['access_token']
+                            """Storing token in a text file so it can be retrieved from other class for an hour"""
+                            with open("Access_Token_Text_Read", "w") as access_token_text_read:
+                                access_token_text_read.write(access_token_read)
+                        case 201:
+                            print('Status Code: Created')
+                        case 204:
+                            print('Status Code: No content')
+                        case 400:
+                            print('Status Code: Bad request')
+                        case 401:
+                            print('Status Code: Unauthorized')
+                        case 403:
+                            print('Status Code: Forbidden (Unauthorised access)')
+                        case 404:
+                            print('Status Code: URL not found')
+                        case 405:
+                            print('Status Code: Method not allowed (Method called is not supported for the API invoked)')
+                        case 413:
+                            print('Status Code: Payload Too Large')
+                        case 415:
+                            print('Status Code: Unsupported Media Type')
+                        case 422:
+                            print('Status Code: Unprocessable Entity')
+                        case 429:
+                            print('Status Code: Too Many Requests')
+                        case 500:
+                            print('Status Code: Internal error')
+                case {'scope': 'Desk.tickets.UPDATE'}:
+                    self.update_access_token_params.update(self.scope)
+                    response = requests.post(self.update_access_token_url, self.update_access_token_params)
+                    statuscode = response.status_code
+                    jsondata = response.json()
+                    match statuscode:
+                        case 200:
+                            print('Status Code: OK')
+                            access_token_update = jsondata['access_token']
+                            """Storing token in a text file so it can be retrieved from other class for an hour"""
+                            with open("Access_Token_Text_Update", "w") as access_token_text_update:
+                                access_token_text_update.write(access_token_update)
+                        case 201:
+                            print('Status Code: Created')
+                        case 204:
+                            print('Status Code: No content')
+                        case 400:
+                            print('Status Code: Bad request')
+                        case 401:
+                            print('Status Code: Unauthorized')
+                        case 403:
+                            print('Status Code: Forbidden (Unauthorised access)')
+                        case 404:
+                            print('Status Code: URL not found')
+                        case 405:
+                            print('Status Code: Method not allowed (Method called is not supported for the API invoked)')
+                        case 413:
+                            print('Status Code: Payload Too Large')
+                        case 415:
+                            print('Status Code: Unsupported Media Type')
+                        case 422:
+                            print('Status Code: Unprocessable Entity')
+                        case 429:
+                            print('Status Code: Too Many Requests')
+                        case 500:
+                            print('Status Code: Internal error')
+                case {'scope' : 'Desk.tickets.ALL'}:
+                    self.read_access_token_params.update(self.scope)
+                    response = requests.post(self.read_access_token_url, self.read_access_token_params)
+                    statuscode = response.status_code
+                    jsondata = response.json()
+                    match statuscode:
+                        case 200:
+                            print('Status Code: OK')
+                            access_token_all = jsondata['access_token']
+                            """Storing token in a text file so it can be retrieved from other class for an hour"""
+                            with open("Access_Token_Text_All", "w") as access_token_text_all:
+                                access_token_text_all.write(access_token_all)
+                        case 201:
+                            print('Status Code: Created')
+                        case 204:
+                            print('Status Code: No content')
+                        case 400:
+                            print('Status Code: Bad request')
+                        case 401:
+                            print('Status Code: Unauthorized')
+                        case 403:
+                            print('Status Code: Forbidden (Unauthorised access)')
+                        case 404:
+                            print('Status Code: URL not found')
+                        case 405:
+                            print('Status Code: Method not allowed (Method called is not supported for the API invoked)')
+                        case 413:
+                            print('Status Code: Payload Too Large')
+                        case 415:
+                            print('Status Code: Unsupported Media Type')
+                        case 422:
+                            print('Status Code: Unprocessable Entity')
+                        case 429:
+                            print('Status Code: Too Many Requests')
+                        case 500:
+                            print('Status Code: Internal error')
+                case _:
+                    print("Logic for this scope hasnt been set up")
+
+
+        def call_refresh_token(self):
+            print("Refresh token method is a work in progress")
+
+
+        def revoke_token(self):
+            print("Revoke token method is a work in progress")
+
